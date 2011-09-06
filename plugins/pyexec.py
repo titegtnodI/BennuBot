9ba8a5b4855c32be68443cba1f78dce1043b2e44
ti.teg.tnod.I <ti.teg.tnod.I@gmail.com>
2011-09-06 13:56:31 -0700

    No longer uses \
    \
    \
    \\

--- plugins/pyexec.py
+++ plugins/pyexec.py
@@ -9,7 +9,7 @@ def localPythonExec(inMSG):
 		outMSG += [['Not authorized.', inMSG[1], inMSG[2], inMSG[3]]]
 		return
 	try:
-		exec inMSG[0][len(inMSG[0].split()[0]) + 1:]
+		exec inMSG[0][inMSG[0].index(' ')+1:]
 		outMSG += [['Done.', inMSG[1], inMSG[2], inMSG[3]]]
 	except Exception as e:
 		outMSG += [[str(e), inMSG[1], inMSG[2], inMSG[3]]]
--- plugins/say.py
+++ plugins/say.py
@@ -2,7 +2,7 @@ plugName = 'Say'
 
 def say_say(inMSG):
 	global outMSG
-	outMSG += [[inMSG[0][len(inMSG[0].split()[0])+1:], inMSG[1], inMSG[2], inMSG[3]]]
+	outMSG += [[inMSG[0][inMSG[0].index(' ')+1:], inMSG[1], inMSG[2], inMSG[3]]]
 
 def load():
 	return {'say':say_say}
