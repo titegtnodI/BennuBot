7d8334d7b15f9f197949480cc5f3384f0f48038b
ti.teg.tnod.I <ti.teg.tnod.I@gmail.com>
2011-09-28 19:04:16 -0700

    Demonstrates multiple commands for 1 plugin

--- plugins/say.py
+++ plugins/say.py
@@ -4,5 +4,9 @@ def say_say(inMSG):
 	global outMSG
 	outMSG.append([inMSG[0].split(None, 1)[1], inMSG[1], inMSG[2], inMSG[3]])
 
+def say_reverse(inMSG):
+	global outMSG
+	outMSG.append([inMSG[0].split(None, 1)[1][::-1], inMSG[1], inMSG[2], inMSG[3]])
+
 def load():
-	return {'say':say_say}
+	return {'say':say_say, 'rev':say_reverse}
