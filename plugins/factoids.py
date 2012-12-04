d5875fdde35f0065c8caee7e7e499697940e03c5
Ryan Baumann <ti.teg.tnod.I@gmail.com>
2012-12-04 00:16:51 -0500

    Made some changes / upgrades to factoids

--- plugins/factoids.py
+++ plugins/factoids.py
@@ -1,5 +1,7 @@
 #Don't care if factoids isn't a word.
 #Enjoy the plugin.
+#TODO Implement <action> better
+#TODO Implement $inp$
 plugName = 'Factoids'
 
 fact_prefix = '\''
@@ -17,11 +19,12 @@ def fact_remember(inMSG):
     if getSetting('Facts', splitMSG[1]):
         return 'Factoid "'+splitMSG[1]+'" already exists.'
 
-    if len(splitMSG[2]) < 9 or splitMSG[2][:7] != '<reply>':
+    if len(splitMSG[2]) < 9 or (splitMSG[2][:7] != '<reply>' and (splitMSG[2][:8] != '<action>' or
+       inMSG[1] != 'irc')):
         return 'Factoid doesn\'t begin with "<reply>".'
 
     setSetting('Facts', splitMSG[1], (splitMSG[2],))
-    sendMSG('"'+splitMSG[1]+'" added.', inMSG[1], inMSG[2], inMSG[3])
+    return '"'+splitMSG[1]+'" added.'
 
 def fact_forget(inMSG):
     if getPermission(inMSG) < fact_setPermissions:
@@ -34,7 +37,7 @@ def fact_forget(inMSG):
     if not delSetting('Facts', splitMSG[1]):
         return 'Error deleting "'+splitMSG[1]+'" (Probably doesn\'t exist).'
 
-    sendMSG('"'+splitMSG[1]+'" deleted.', inMSG[1], inMSG[2], inMSG[3])
+    return '"'+splitMSG[1]+'" deleted.'
 
 def fact_replace(inMSG):
     if getPermission(inMSG) < fact_setPermissions:
@@ -47,11 +50,12 @@ def fact_replace(inMSG):
     if not getSetting('Facts', splitMSG[1]):
         return 'Factoid "'+splitMSG[1]+'" does not exist.'
 
-    if len(splitMSG[2]) < 9 or splitMSG[2][:7] != '<reply>':
+    if len(splitMSG[2]) < 9 or (splitMSG[2][:7] != '<reply>' and (splitMSG[2][:8] != '<action>' or
+       inMSG[1] != 'irc')):
         return 'Factoid doesn\'t begin with "<reply>".'
 
     setSetting('Facts', splitMSG[1], (splitMSG[2],))
-    sendMSG('"'+splitMSG[1]+'" replaced.', inMSG[1], inMSG[2], inMSG[3])
+    return '"'+splitMSG[1]+'" replaced.'
 
 def fact_getFact(inMSG):
     if (not inMSG or len(inMSG) != 6 or getPermission(inMSG) < fact_usePermissions or
@@ -64,10 +68,14 @@ def fact_getFact(inMSG):
         return
     
     fact = getSetting('Facts', inMSG[0][len(fact_prefix):])
-    if not fact or len(fact[0][1]) < 9 or fact[0][1][:7] != '<reply>':
+    if not fact or len(fact[0][1]) < 9 or (fact[0][1][:7] != '<reply>' and (fact[0][1][:8] != '<action>' or
+       inMSG[1] != 'irc')):
         return
 
-    sendMSG(fact[0][1][7:], inMSG[1], inMSG[2], inMSG[3])
+    if fact[0][1][:2] == '<a':
+        sendMSG('\x01ACTION ' + fact[0][1][8:] + '\x01', inMSG[1], inMSG[2], inMSG[3])
+    else:
+        return sendMSG(fact[0][1][7:], inMSG[1], inMSG[2], inMSG[3])
 
 def load():
     global funcs
