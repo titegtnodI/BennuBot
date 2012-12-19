cf106fa48f54679f7fe281b803d9ffb7955cc6f1
Ryan Baumann <ti.teg.tnod.I@gmail.com>
2012-12-19 04:11:15 -0500

    Tabs to spaces. Usage outputs now. closes #10

--- plugins/say.py
+++ plugins/say.py
@@ -1,14 +1,18 @@
 plugName = 'Say'
 
 def say_say(inMSG):
-	msg = inMSG[0].split(None, 1)
-	if len(msg) > 1:
-		sendMSG(msg[1], inMSG[1], inMSG[2], inMSG[3])
+    msg = inMSG[0].split(None, 1)
+    if len(msg) > 1:
+        return msg[1]
+    else:
+        return 'Usage: ' + funcPrefix + 'say <msg>'
 
 def say_reverse(inMSG):
-	msg = inMSG[0].split(None, 1)
-	if len(msg) > 1:
-		sendMSG(msg[1][::-1], inMSG[1], inMSG[2], inMSG[3])
+    msg = inMSG[0].split(None, 1)
+    if len(msg) > 1:
+        return msg[1][::-1]
+    else:
+        return 'Usage: ' + funcPrefix + 'rev <msg>'
 
 def load():
-	return {'say':say_say, 'rev':say_reverse}
+    return {'say':say_say, 'rev':say_reverse}
