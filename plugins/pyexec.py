4f7c6fc56acf98b2a70a4dd3b1dda4217ecec0f5
Ryan Baumann <ti.teg.tnod.I@gmail.com>
2013-01-25 03:11:57 -0500

    Updated this plugin for newer standards. It was archaic.

--- plugins/pyexec.py
+++ plugins/pyexec.py
@@ -1,14 +1,13 @@
 plugName = 'Local Python Exec'
 
-def load():
-	return {'pyexec':localPythonExec}
-
 def localPythonExec(inMSG):
-	if getPermission(inMSG) < 1000:
-		outMSG.append(['Not authorized.', inMSG[1], inMSG[2], inMSG[3]])
-		return
-	try:
-		exec inMSG[0].split(None, 1)[1]
-		sendMSG('Done.', inMSG[1], inMSG[2], inMSG[3])
-	except Exception as e:
-		sendMSG(str(e), inMSG[1], inMSG[2], inMSG[3])
+    if getPermission(inMSG) < 1000:
+        return 'Not authorized.'
+    try:
+        exec inMSG[0].split(None, 1)[1]
+        return 'Done.'
+    except Exception as e:
+        return str(e)
+
+def load():
+    return {'pyexec':localPythonExec}
